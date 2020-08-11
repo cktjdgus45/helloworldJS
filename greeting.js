e2c6a734857c8e4ebdee8142ed0d03467ef1b0f0
cktjdgus45 <cktjdgus45@naver.com>
2020-08-11 22:07:20 +0900

    Saving the User Name part2

--- greeting.js
+++ greeting.js
@@ -6,15 +6,32 @@ const greeting = document.querySelector(".js-greeting");
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
   greeting.innerHTML = `Hello ${text}`;
 }
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const currnetValue = input.value;
+  paintGreeting(currnetValue);
+  saveName(currnetValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
