0c1494850eed16c90f660b71d5683b0c1b778354
cktjdgus45 <cktjdgus45@naver.com>
2020-08-12 16:49:45 +0900

    toDoList deletBTN

--- todo.js
+++ todo.js
@@ -4,7 +4,7 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
 
 function loadToDos() {
   const loadedtoDos = localStorage.getItem(TODOS_LS);
@@ -20,9 +20,21 @@ function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDo = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDo;
+  saveToDos();
+}
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
+  delBtn.addEventListener("click", deleteToDo);
   delBtn.innerHTML = "x";
   const span = document.createElement("span");
   const newId = toDos.length + 1;
